1c9f3495fef1b3252355c80cf96abf58f901f64a
ArrowX17 <iamsamix17@gmail.com>
2020-05-12 16:16:10 +0600

    Route separated

---
+++ node_express_proj/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const speakersRoute = require('./speakers');
+const feedbackRoute = require('./feedback');
+
+const router = express.Router();
+
+module.exports = () => {
+  router.get('/', (request, response) => {
+    response.render('pages/index', { pageTitle: 'Welcome' });
+  });
+
+  router.use('/feedback', feedbackRoute());
+  router.use('/speakers', speakersRoute());
+
+  return router;
+};
--- node_express_proj/server.js
+++ node_express_proj/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
+const routes = require('./routes');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+app.use(express.static('static'));
+app.use('/', routes());
 
 // Global variables
 const PORT = 3000;
 
-app.get('/', (request, response) => {
-  response.send("Hello");
-});
-
-app.listen(3000, () => console.log(`Server is listening on port ${PORT}`))
+app.listen(3000, () => console.log(`Server is listening on port ${PORT}`));
